46eec83011e8d08c6ca29297e6724ef7b2f8fbed
joker7t <batoan131196@gmail.com>
2020-11-06 22:59:11 +0700

    basic event bus workflow

--- event-bus/index.js
+++ event-bus/index.js
@@ -9,12 +9,12 @@ app.use(bodyParser.json());
 app.use(cors());
 
 app.post('/events', (req, res) => {
-    const {event} = res.body;
+    const event = req.body;
     axios.post('http://localhost:4000/events', event);
     axios.post('http://localhost:4001/events', event);
-    axios.post('http://localhost:4002/events', event);
+    // axios.post('http://localhost:4002/events', event);
 
-    res.status({status: 'OK'});
+    res.send({status: 'OK'});
     
 });
 
