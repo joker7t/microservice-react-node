bf9c5a1ade7b191cf2a1e030325c38dc335082c2
joker7t <batoan131196@gmail.com>
2020-11-24 23:14:59 +0700

    add api for signup

--- ticketing/auth/src/index.ts
+++ ticketing/auth/src/index.ts
@@ -13,10 +13,6 @@ app.use(signInRouter);
 app.use(signOutRouter);
 app.use(signUpRouter);
 
-app.get('/api/users/currentuser', (req, res) => {
-    res.send('Hi there!');
-});
-
 app.listen(3000, () => {
     console.log("Listening on port 3000!!!");
 });
