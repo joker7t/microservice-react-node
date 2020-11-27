e60080850a65685090a790ac1ea737efa85104c8
joker7t <batoan131196@gmail.com>
2020-11-27 21:51:14 +0700

    add package to handle async errors

--- ticketing/auth/src/index.ts
+++ ticketing/auth/src/index.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import 'express-async-errors';
 import { json } from 'body-parser';
 import {currentUserRouter} from './routes/current-user';
 import {signInRouter} from './routes/signin';
@@ -17,7 +18,7 @@ app.use(signInRouter);
 app.use(signOutRouter);
 app.use(signUpRouter);
 //handle invalid routes
-app.get('*', () => {
+app.get('*', async () => {
     throw new NotFoundError();
 });
 //end routers
