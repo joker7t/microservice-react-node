59c3bc2207c5e7e16c309c2b10026b19707f25c5
joker7t <batoan131196@gmail.com>
2020-11-28 15:12:56 +0700

    connect to mongodb

--- ticketing/auth/src/index.ts
+++ ticketing/auth/src/index.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import 'express-async-errors';
+import mongoose from 'mongoose';
 import { json } from 'body-parser';
 import {currentUserRouter} from './routes/current-user';
 import {signInRouter} from './routes/signin';
@@ -25,6 +26,22 @@ app.get('*', async () => {
 
 app.use(errorHandler);
 
+const start = async () => {
+    try {
+        await mongoose.connect('mongodb://auth-mongo-serv:27017/auth', {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useCreateIndex: true
+        });
+        console.log('Connected to mongodb...');
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+start();
+
 app.listen(3000, () => {
     console.log("Listening on port 3000!!!");
 });
+
