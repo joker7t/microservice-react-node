51a255901682742972e23464539da8014f6966d7
joker7t <batoan131196@gmail.com>
2020-12-01 19:41:00 +0700

    extract handle validate request to middleware

--- ticketing/auth/src/routes/signin.ts
+++ ticketing/auth/src/routes/signin.ts
@@ -1,6 +1,6 @@
 import express, {Request, Response} from 'express';
-import { body, validationResult } from 'express-validator';
-import { RequestValidationError } from '../errors/request-validation-error';
+import { body } from 'express-validator';
+import {validateRequest} from '../middlewares/validate-request';
 
 const router = express.Router();
 
@@ -9,13 +9,8 @@ router.post('/api/users/signin',
         body('email').isEmail().withMessage('Email must be valid'),
         body('password').trim().isLength({min: 4, max: 20}).withMessage('Password must be between 4 and 20 characters')
     ],
+    validateRequest,
     (req: Request, res: Response) => {
-        const errors = validationResult(req);
-        if (!errors.isEmpty()) {
-            throw new RequestValidationError(errors.array());
-        }
-        // throw new DatabaseConnectionError();
-
         const {email, password} : {email:string, password:string} = req.body;
 
         res.send('Hi there');
