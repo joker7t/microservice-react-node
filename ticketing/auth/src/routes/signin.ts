30ce041a7b50021bb50b2c39ed46d831c359e475
joker7t <batoan131196@gmail.com>
2020-12-01 19:32:21 +0700

    basic for sign in

--- ticketing/auth/src/routes/signin.ts
+++ ticketing/auth/src/routes/signin.ts
@@ -1,9 +1,24 @@
-import express from 'express';
+import express, {Request, Response} from 'express';
+import { body, validationResult } from 'express-validator';
+import { RequestValidationError } from '../errors/request-validation-error';
 
 const router = express.Router();
 
-router.post('/api/users/signin', (req, res) => {
-    res.send('Hi there');
+router.post('/api/users/signin', 
+    [
+        body('email').isEmail().withMessage('Email must be valid'),
+        body('password').trim().isLength({min: 4, max: 20}).withMessage('Password must be between 4 and 20 characters')
+    ],
+    (req: Request, res: Response) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            throw new RequestValidationError(errors.array());
+        }
+        // throw new DatabaseConnectionError();
+
+        const {email, password} : {email:string, password:string} = req.body;
+
+        res.send('Hi there');
 });
 
 export {router as signInRouter};
