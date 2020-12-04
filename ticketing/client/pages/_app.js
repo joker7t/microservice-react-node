212712918897cf5b3489d18fce18cc696c5aee03
joker7t <batoan131196@gmail.com>
2020-12-04 22:16:02 +0700

    custom head nextjs

--- ticketing/client/pages/_app.js
+++ ticketing/client/pages/_app.js
@@ -1,7 +1,13 @@
 import 'bootstrap/dist/css/bootstrap.css';
+import Head from 'next/head';
 
 const _app = ({ Component, pageProps }) => {
-    return <Component {...pageProps} />
+    return <div>
+        <Head>
+            <title>Custom joker7nbt.dev</title>
+        </Head>
+        <Component {...pageProps} />
+    </div>
 }
 
 export default _app;
